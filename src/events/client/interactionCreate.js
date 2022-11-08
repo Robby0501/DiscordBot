f633007c941a3e2713c7d948085005d20597f343
Robby0501 <robbyulrich@hotmail.com>
2022-11-08 15:39:23 -0700

    handle commands

--- src/events/client/interactionCreate.js
+++ src/events/client/interactionCreate.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'interactionCreate',
     async execute(interaction, client) {
-        if (interaction.isCommand()) {
+        if (interaction.isChatInputCommand()) {
             const { commands } = client;
             const { commandName } = interaction;
             const command = commands.get(commandName);
--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -1,3 +1,5 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const fs = require("fs");
 
 module.exports = (client) => {
@@ -14,7 +16,21 @@ module.exports = (client) => {
            commands.set(command.data.name, command);
            commandArray.push(command.data.toJSON());
      }
+    }
 
+    const clientId = '1039647352362893332';
+    const guildId = '105810342478630912';
+    const rest = new REST({ version: "9"}).setToken(process.env.token);
+    try {
+      console.log("Started refreshing application (/) commands.")
+
+      await rest.put(Routes.applicationGuildCommands(clientId, guildId),{
+        body: commandArray,
+      });
+
+      console.log("Successfully reloaded application (/) commands.")
+    } catch (error) {
+      console.error(error);
     }
   };
 };
